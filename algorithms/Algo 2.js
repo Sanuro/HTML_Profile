62caadc1841403407e71bbf2db798e4dd4e531eb
Jaewon Lee <j.1.@Jaewons-MacBook-Pro.local>
2018-05-31 13:17:20 -0700

    5/31/2018 clicker prototype portfolio

---
+++ algorithms/Algo 2.js
@@ -0,0 +1,6 @@
+function printUpTo(x){
+  // your code here
+}
+printUpTo(1000000); // should print all the integers from 1 to 1000000
+y = printUpTo(-10); // should return false
+console.log(y); // should print false
